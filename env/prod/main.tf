5ce47c85f8044089917c1cc7a8c06949bf122cc8
imsung <isyou7487@naver.com>
2023-10-24 15:46:45 +0900

    Feat: prod 환경 vpc 추가

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -45,3 +45,135 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     type = "S"
   }
 }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_name            = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  azs                 = var.azs
+  vpc_public_subnets  = var.vpc_public_subnets
+  public_subnet_tags  = var.public_subnet_tags
+  vpc_private_subnets = var.vpc_private_subnets
+  private_subnet_tags = var.private_subnet_tags
+
+  enable_nat_gateway     = var.enable_nat_gateway
+  single_nat_gateway     = var.single_nat_gateway
+  one_nat_gateway_per_az = var.one_nat_gateway_per_az
+
+  tags = var.tags
+}
+
+data "aws_vpc" "dev" {
+  tags = {
+    Environment = "dev"
+  }
+}
+
+locals {
+  dev_vpc_id = data.aws_vpc.dev.id
+}
+
+resource "aws_vpc_peering_connection" "dev-prod-peering" {
+  peer_vpc_id = data.aws_vpc.dev.id
+  vpc_id      = module.vpc.vpc_id
+  auto_accept = true
+}
+
+module "eks" {
+  source = "../../modules/eks"
+
+  vpc_id = module.vpc.vpc_id
+
+  cluster_name                   = var.cluster_name
+  cluster_version                = var.cluster_version
+  cluster_endpoint_public_access = var.cluster_endpoint_public_access
+
+  cluster_addons = merge(var.cluster_addons, {
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+    }
+  })
+
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = var.eks_managed_node_groups
+
+  tags = var.tags
+}
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "AmazonEKS_EBS_CSI_DriverRole_PROD"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = var.tags
+}
+
+module "load_balancer_controller_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                              = "aws-load-balancer-controller-prod"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "aws-load-balancer-controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.load_balancer_controller_irsa_role.iam_role_arn
+    }
+  }
+}
+
+resource "helm_release" "aws-load-balancer-controller" {
+  depends_on = [module.load_balancer_controller_irsa_role]
+  name       = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  namespace = "kube-system"
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.ap-northeast-2.amazonaws.com/amazon/aws-load-balancer-controller" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+}
---
+++ env/prod/variables.tf
@@ -0,0 +1,80 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+### VPC
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "Default value for VPC CIDR"
+  type        = string
+}
+
+variable "azs" {
+  description = "Availability zones for the region"
+  type        = list(string)
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for the VPC"
+  type        = list(string)
+}
+
+variable "public_subnet_tags" {
+  type = map(string)
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for the VPC"
+  type        = list(string)
+}
+
+variable "private_subnet_tags" {
+  type = map(string)
+}
+
+variable "enable_nat_gateway" {
+  type = bool
+}
+
+variable "single_nat_gateway" {
+  type = bool
+}
+
+variable "one_nat_gateway_per_az" {
+  type = bool
+}
+
+# ### ECR
+# variable "repository_names" {
+#   type = list(string)
+# }
+
+### EKS
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "cluster_endpoint_public_access" {
+  type = bool
+}
+
+variable "cluster_addons" {
+  type = map(any)
+}
+
+variable "eks_managed_node_groups" {
+  type = map(any)
+}
